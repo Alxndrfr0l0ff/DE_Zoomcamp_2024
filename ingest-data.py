22d7767fa00f5c9529469b78a7e06c26a676f1f5
Oleksandr Frolov <33040050+Alxndrfr0l0ff@users.noreply.github.com>
2024-01-28 10:15:54 +0000

    renamed upload-data to ingest-data

--- ingest-data.py
+++ ingest-data.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import pandas as pd
 import pyarrow.parquet as pq
-
-
+from sqlalchemy import create_engine
 
 
 df = pq.read_table('yellow_tripdata_2021-01.parquet')
 trips = df.to_pandas()
-
-from sqlalchemy import create_engine
 
 conn = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
@@ -21,5 +16,7 @@ trips.tpep_dropoff_datetime = pd.to_datetime(trips.tpep_dropoff_datetime)
 
 print(pd.io.sql.get_schema(trips, name='yellow_taxi_data', con=conn))
 
-trips.to_sql(name='yellow_taxi_data', con=conn, if_exists='append')
+trips.head(0).to_sql(name='yellow_taxi_data', con=conn, if_exists='replace')
+
+
 
